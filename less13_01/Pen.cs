8abcf74049db1c395ba042568f0ad7bdbf9e8666
MSR <mazitov@ukr.net>
2017-11-15 20:49:08 +0200

    Add project files.

---
+++ less13_01/Pen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less13_01
+{
+    public class Pen
+    {
+        public const int Size = 668;
+        public int Weight;
+        private int _x;
+
+        private string _manufacturer;
+
+        public int MyProperty { get; set; }
+
+        public int SizePen { get; set; }
+
+        private double _price;
+
+        public double Price => _price;
+
+        public string Manufacturer
+        {
+            get { return _manufacturer;  }
+            set { _manufacturer = value; }
+        }
+
+        //public Pen()
+        //{
+        //    int x = 99;
+        //    this._x = x;
+        //}
+
+        public Pen(string manufacturer)
+        {
+            _manufacturer = manufacturer;
+        }
+
+    }
+}
---
+++ less13_02/less13_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using less13_01;
+
+namespace less13_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Pen.Size);
+
+            var pen = new Pen("RRRiii");
+            //pen.Price = 899;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("йцукен");
+            Console.WriteLine(pen.Manufacturer);
+            Console.WriteLine(pen.Price);
+
+            Console.ReadKey();
+        }
+    }
+}
